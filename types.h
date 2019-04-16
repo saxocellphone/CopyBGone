3f58b60c9b7c65f7108f007acc2c3a08ba34ca87
Tim Clough <tmclough98@gmail.com>
2019-04-16 19:49:24 -0400

    add types.h

---
+++ types.h
@@ -0,0 +1,11 @@
+#ifndef GUARD_types_h
+#define GUARD_types_h
+
+typedef int hash_t;
+typedef long position_t;
+typedef struct _location_t {
+    position_t pos;
+    char * source_file;
+} location_t;
+
+#endif
