2824f97134c7db537d263a17954a5d91150d83ca
Victor Nazzaro <victor741naruto@gmail.com>
2019-04-16 21:48:56 -0400

    Added size counter and some documentation

--- fingerprints.c
+++ fingerprints.c
@@ -3,7 +3,11 @@
 void fingerprints_create(table_t* table) {
     table->buckets = (node_t*) malloc(SIZE * sizeof(struct _node_t));
 }
-
+/**
+ * Gets a linked list of all the locations of a fingerprint
+ * 
+ * Returns 0 if fingerprint isn't found, returns 1 if it is found.
+ * */
 int fingerprints_get(table_t* table, unsigned int hash, location_list_t* locations_list) {
     unsigned int bucket_id = hash_it(hash);
     node_t* curr = table->buckets[bucket_id];
@@ -16,18 +20,29 @@ int fingerprints_get(table_t* table, unsigned int hash, location_list_t* locatio
     return 0;
 }
 
+/**
+ * Adds a fingerprint to the list of known fingerprints.
+ * 
+ * Returns 0 if it is a new fingerprint, returns 1 if it is an existing fingerprint.
+ * */
 int fingerprints_add(table_t* table, unsigned int hash) {
     unsigned int bucket_id = hash_it(hash);
     location_list_t* locations_list;
     if(fingerprints_get(table, hash, locations_list) == 1){
         location_node_t* new_location = (location_node_t*) malloc(sizeof(location_node_t));
-        locations_list->tail->next = new_location;
-        locations_list->tail = new_location;
+        if(locations_list->size == 0){
+            locations_list->head = locations_list->ptr = locations_list->tail = new_location;
+        } else {
+            locations_list->tail->next = new_location;
+            locations_list->tail = new_location;
+        }
+        locations_list->size++;
         return 1;
     } else {
         node_t* new_node = (node_t*) malloc(sizeof(struct _node_t));
         new_node->hash = hash;
         locations_list = (location_list_t*) malloc(sizeof(location_list_t));
+        locations_list->size = (int*) calloc(1, sizeof(int));
         new_node->locs = (location_t*) malloc(sizeof(struct _location_t));
         node_t* curr = table->buckets[bucket_id];
         while(curr){
